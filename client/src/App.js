b51475cbd62dc352e8a8e8c5494d065b9389de32
Peter Roto <peterdroto@gmail.com>
2021-08-28 20:11:39 -0500

    had to recrete the react app and copy paste previous work, unclear webpack error.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
--- server/server.js
+++ server/server.js
@@ -3,17 +3,15 @@ const path = require('path');
 const db = require('./config/connection');
 // const routes = require('./routes');
 
+// importing ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
-const { typeDefs, resolvers } = require('./schemas');
+// importing typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas')
 const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 const server = new ApolloServer({
     typeDefs,
@@ -21,18 +19,24 @@ const server = new ApolloServer({
     context: authMiddleware
 })
 
-server.applyMiddleware({ app })
+// applying Apollo middleware here
+server.applyMiddleware({ app });
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/public')));
 }
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+})
 
 // app.use(routes);
+
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`🌍 Now listening on localhost:${PORT}`)
