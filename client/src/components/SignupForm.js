6cc5e27584b7d8c7512b41be36527796f858e101
Peter Roto <peterdroto@gmail.com>
2021-08-28 21:04:54 -0500

    Non working commit, but I had to. Fixed a bunch of dead connections in mutations

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,9 +11,12 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  // sets up add user mutation hook to call the add user mutation
+  const [addUser, {error}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+    console.log(name, value)
     setUserFormData({ ...userFormData, [name]: value });
   };
 
@@ -28,14 +31,16 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: userFormData});
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
+      console.log(data)
 
-      const { token, user } = await response.json();
-      console.log(user);
+      const token = data.addUser.token;
+      // console.log(user);
       Auth.login(token);
     } catch (err) {
       console.error(err);
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,30 @@
 import gql from 'graphql-tag';
 
-export const LOGIN_USER = gql `
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
