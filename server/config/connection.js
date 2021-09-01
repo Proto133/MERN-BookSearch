86d47b324e924ba0c19836c21702d3edc8ab0dca
Peter Roto <peterdroto@gmail.com>
2021-09-01 16:32:09 -0500

    update minor config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
+console.log(mongoose.connection)
 module.exports = mongoose.connection;
