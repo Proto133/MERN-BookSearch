72c27dcc9f077e796c43a444ea8cdbe527a8652f
Peter Roto <peterdroto@gmail.com>
2021-09-03 00:20:25 -0500

    heroku attempt 1111127- changed server.js file

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     // app.use(express.static(path.join(__dirname, '../client/public')));
 }
 // console.log(path.join(__dirname, '../client/build', 'index.html'))
