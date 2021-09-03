16c4b3b9cf860e6201f6bb0c42c67ffd2564b5da
Peter Roto <peterdroto@gmail.com>
2021-09-02 23:54:35 -0500

    heroku attempt 1111124

--- server/server.js
+++ server/server.js
@@ -29,8 +29,10 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/public')));
+    app.use(express.static(path.join(__dirname, 'build')));
+    // app.use(express.static(path.join(__dirname, '../client/public')));
 }
+// console.log(path.join(__dirname, '../client/build', 'index.html'))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
